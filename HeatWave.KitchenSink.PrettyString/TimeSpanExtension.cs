47ca0acca8d562ef3d73519eab502e9af11ac8c3
ThomasRichter <Thomas.M.Richter@gmx.at>
2019-01-09 23:58:37 +0100

    Add project files.

---
+++ HeatWave.KitchenSink.PrettyString/TimeSpanExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HeatWave.KitchenSink.PrettyString
+{
+    public static class TimeSpanExtension
+    {
+        private static readonly TimeSpan OneSecond = TimeSpan.FromSeconds(1);
+        private static readonly TimeSpan OneHundredMillis = TimeSpan.FromMilliseconds(100);
+        private static readonly TimeSpan OneMinute = TimeSpan.FromMinutes(1);
+        private static readonly TimeSpan OneHour = TimeSpan.FromHours(1);
+
+        public static string AsPrettyString(this TimeSpan timeSpan)
+        {
+            if (timeSpan > OneHour)
+            {
+                return $"{Math.Floor(timeSpan.TotalHours):0} hours, {timeSpan.Minutes} minutes";
+            }
+
+            if (timeSpan > OneMinute)
+            {
+                return $"{Math.Floor(timeSpan.TotalMinutes):0} minutes, {timeSpan.Seconds} seconds";
+            }
+
+            if (timeSpan > OneSecond)
+            {
+                return $"{timeSpan.TotalSeconds:0.0} seconds";
+            }
+
+            if (timeSpan > OneHundredMillis)
+            {
+                return $"{Math.Round(timeSpan.TotalMilliseconds):0} millis";
+            }
+
+            return $"{timeSpan.TotalMilliseconds:0.0} millis";
+        }
+    }
+}
