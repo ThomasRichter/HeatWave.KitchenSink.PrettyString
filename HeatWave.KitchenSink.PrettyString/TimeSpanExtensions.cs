ea0159262e952a6eaca0fd9c47ec031d16b98202
ThomasRichter <Thomas.M.Richter@gmx.at>
2019-01-12 03:46:27 +0100

    Improved ToPrettyString for TimeSpan, added demo project and UnitTests for TimeSpan

--- HeatWave.KitchenSink.PrettyString/TimeSpanExtensions.cs
+++ HeatWave.KitchenSink.PrettyString/TimeSpanExtensions.cs
@@ -4,34 +4,71 @@ namespace HeatWave.KitchenSink.PrettyString
 {
     public static class TimeSpanExtensions
     {
+        private static readonly TimeSpan OneHour = TimeSpan.FromHours(1);
+        private static readonly TimeSpan OneMinute = TimeSpan.FromMinutes(1);
         private static readonly TimeSpan OneSecond = TimeSpan.FromSeconds(1);
         private static readonly TimeSpan OneHundredMillis = TimeSpan.FromMilliseconds(100);
-        private static readonly TimeSpan OneMinute = TimeSpan.FromMinutes(1);
-        private static readonly TimeSpan OneHour = TimeSpan.FromHours(1);
+        private static readonly TimeSpan TenMillis = TimeSpan.FromMilliseconds(10);
 
+        /// <summary>
+        /// Returns a string using the appropriate unit to display 3 or 4 significant digits.
+        /// </summary>
+        /// <param name="timeSpan"></param>
+        /// <returns></returns>
         public static string ToPrettyString(this TimeSpan timeSpan)
         {
-            if (timeSpan > OneHour)
+            var absTimeSpan = timeSpan >= TimeSpan.Zero
+                ? timeSpan
+                : timeSpan.Negate();
+
+            // '0 millis' through '0.99 millis' (for up to 0.994) 
+            // '1 milli' (for 0.995 through 1.004)
+            // '1.01 millis' (for 1.005 and up)
+            // '9.99 millis'
+            if (absTimeSpan < TenMillis)
             {
-                return $"{Math.Floor(timeSpan.TotalHours):0} hours, {timeSpan.Minutes} minutes";
+                return $"{timeSpan.TotalMilliseconds:0.##} {(RoundsToSingleMilli(absTimeSpan) ? "milli" : "millis")}";                
             }
 
-            if (timeSpan > OneMinute)
+            // '10 millis' through '99.9 millis'
+            if (absTimeSpan < OneHundredMillis)
             {
-                return $"{Math.Floor(timeSpan.TotalMinutes):0} minutes, {timeSpan.Seconds} seconds";
+                return $"{timeSpan.TotalMilliseconds:0.#} millis";               
             }
 
-            if (timeSpan > OneSecond)
+            // '100 millis' through '999 millis'
+            if (absTimeSpan < OneSecond)
             {
-                return $"{timeSpan.TotalSeconds:0.0} seconds";
+                return $"{timeSpan.TotalMilliseconds:0} millis";           
             }
 
-            if (timeSpan > OneHundredMillis)
+            // '1 second' through '59.9 seconds'
+            if (absTimeSpan < OneMinute)
             {
-                return $"{Math.Round(timeSpan.TotalMilliseconds):0} millis";
+                return $"{timeSpan.TotalSeconds:0.#} {(RoundsToSingleSecond(absTimeSpan) ? "second" : "seconds")}";
             }
 
-            return $"{timeSpan.TotalMilliseconds:0.0} millis";
+            // '1 minute' through '59 minutes, 59 seconds'
+            if (absTimeSpan < OneHour)
+            {
+                return $"{timeSpan.Minutes} {Pluralize("minute", "minutes", timeSpan.Minutes)}{(timeSpan.Seconds == 0 ? "" : $", {timeSpan.Seconds} {Pluralize("second", "seconds", timeSpan.Seconds)}")}";
+            }
+
+            // '1 hour' ... 1 hour, 5 minutes
+            return $"{timeSpan.Hours} {Pluralize("hour", "hours", timeSpan.Hours)}{(timeSpan.Minutes == 0 ? "" : $", {timeSpan.Minutes} {Pluralize("minute", "minutes", timeSpan.Minutes)}")}";
         }
+
+        private static string Pluralize(string singular, string plural, int number) => Math.Abs(number) == 1 ? singular : plural;
+
+        private static readonly long TicksPerHundrethMilli = TimeSpan.TicksPerMillisecond / 1000;
+
+        private static readonly TimeSpan MillisLowerInclusiveRoundingBound = TimeSpan.FromTicks(TicksPerHundrethMilli * 995);
+        private static readonly TimeSpan MillisUpperExclusiveRoundingBound = TimeSpan.FromTicks(TicksPerHundrethMilli * 1005);
+
+        private static bool RoundsToSingleMilli(TimeSpan timeSpan) => timeSpan >= MillisLowerInclusiveRoundingBound && timeSpan < MillisUpperExclusiveRoundingBound;
+
+        private static readonly TimeSpan SecondsUpperExclusiveRoundingBound = TimeSpan.FromMilliseconds(1500);
+
+        private static bool RoundsToSingleSecond(TimeSpan timeSpan) => timeSpan < SecondsUpperExclusiveRoundingBound;
     }
 }
